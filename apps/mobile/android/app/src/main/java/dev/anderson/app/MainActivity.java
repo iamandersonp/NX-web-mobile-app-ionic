1e65db0fca35b5c40a989d4e937fcc45bf77f015
Anderson Peñaloza <designeromega@gmail.com>
2023-02-26 16:12:41 +0100

    acc capacitor and initial android/ios projects

---
+++ apps/mobile/android/app/src/main/java/dev/anderson/app/MainActivity.java
@@ -0,0 +1,5 @@
+package dev.anderson.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.anderson.app',
+  appName: 'dev.anderson.app',
+  webDir: '../../dist/apps/mobile/',
+  bundledWebRuntime: false
+};
+
+export default config;
